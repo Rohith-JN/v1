9aac1a17d2f29059bebcf92534bab25e80ce6a31
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2023-02-12 14:21:49 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,9 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import About from './pages/About';
-import './App.scss';
-import React, { useEffect } from 'react';
+import './app.scss';
 
 export default function App() {
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  const handleLoading = () => {
-    setIsLoading(false);
-  };
-  window.addEventListener('load', handleLoading);
-
-  useEffect(() => {
-    return () => window.removeEventListener('load', handleLoading);
-  }, []);
-
   return (
       <div>
           <div className="app">
